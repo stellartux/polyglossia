42567bdbf9fd3b331746f82792672704e2ba0061
Danny Robinson <dannyrobinson@gmx.com>
2023-03-07 21:02:48 +0000

    less confusing JS/C hello world

--- hello-world/hello_js.c
+++ hello-world/hello_js.c
@@ -1,4 +1,5 @@
-// ` Code visible to C
+//\
+/* Code visible to C
 
 #ifndef __COSMOPOLITAN__
 #include <stdio.h>
@@ -9,17 +10,19 @@ void sayHello() {
   puts("Hello from \x1b[90mC\x1b[m!");
 }
 
-#if 0 // Code visible to JavaScript `
+#if 0 // Code visible to JavaScript */
 
-const puts = globalThis.print || globalThis.console.log;
+const puts = console.log;
 const sayHello = (s = "Hello from \x1b[33mJavaScript\x1b[m!") => puts(s);
 
-// `
-#endif // Code visible to C and JavaScript `
+//\
+/*
+#endif // Code visible to C and JavaScript */
 
 function main() {
   sayHello();
   puts("Hello from \x1b[90mC\x1b[m and \x1b[33mJavaScript\x1b[m!");
 }
 
-// main();
+//\
+main();
